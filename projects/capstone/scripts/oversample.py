b041ea7a1e6f2013d764c39e1374303dfd74b50e
Patrick Poon <patrickmpoon@gmail.com>
2018-05-26 14:26:44 -0400

    Added project files in preparation for submission

--- projects/capstone/scripts/oversample.py
+++ projects/capstone/scripts/oversample.py
@@ -10,20 +10,6 @@ def lop_off(df, decimal_pct):
 
 def oversample(df, col_name='stop_outcome', outcome_values=['Arrest', 'Summons', 'Ticket', 'Verbal Warning',
                                                             'Written Warning']):
-    # tickets = df.loc[df['stop_outcome'] == 'Ticket']
-    # arrests = df.loc[df['stop_outcome'] == 'Arrest']
-    # summons = df.loc[df['stop_outcome'] == 'Summons']
-    # ww = df.loc[df['stop_outcome'] == 'Written Warning']
-    # vw = df.loc[df['stop_outcome'] == 'Verbal Warning']
-    # multiplier_arrests = tickets.shape[0] // arrests.shape[0]
-    # multiplier_summons = tickets.shape[0] // summons.shape[0]
-    # multiplier_ww = tickets.shape[0] // ww.shape[0]
-    # multiplier_vw = tickets.shape[0] // vw.shape[0]
-    # oversampled = df.append([arrests] * multiplier_arrests, ignore_index=True)
-    # oversampled = oversampled.append([summons] * multiplier_summons, ignore_index=True)
-    # oversampled = oversampled.append([ww] * multiplier_ww, ignore_index=True)
-    # oversampled = oversampled.append([vw] * multiplier_vw, ignore_index=True)
-
     print('Num rows before oversampling: {}'.format(df.shape[0]))
     counts = df[col_name].value_counts()
     largest = counts.nlargest(1)
